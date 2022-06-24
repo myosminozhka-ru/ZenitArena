1908ac104c61a4274c720f7fd469cb924807251b
Гончаров Сергей <Goncharov-5769@main-fer.fnpz.uz>
2022-06-24 11:17:28 +0500

    Доработка

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,14 +1,22 @@
 const MainHeader = class MainHeader {
     constructor(){
         this.isMobileMenuOpened = false;
+        this.isUserOpened = false;
     }
     toogleMobileMenu() {
         this.isMobileMenuOpened = !this.isMobileMenuOpened;
+    }
+    toogleUser() {
+        this.isUserOpened = !this.isUserOpened;
     }
     setEventListener() {
         if (!document.querySelector('.burger__icon')) return;
         document.querySelector('.burger__icon').addEventListener('click', () => {
             this.toogleMobileMenu();
+        });
+        if (!document.querySelector('.user_h__icon')) return;
+        document.querySelector('.user_h__icon').addEventListener('click', () => {
+            this.toogleUser();
         }) 
     }
     init() {
